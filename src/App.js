d42f230686b2f262b997b4fa02a2ba4023840a8b
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-03-19 22:58:25 +0600

    lifting state uphandle click function pass with props square component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React, { useState } from 'react';
-function Square({values}){
-  const [value, setvalue] = useState(null)
-  function handelclck(){
-    console.log('x');
-  }
-  return   <button className="square" onClick={handelclck}>{values}</button>
+function Square({value,onSquareClick}){
+  // const [value, setvalue] = useState(null)
+ 
+  return   <button className="square" onClick={onSquareClick}>{value}</button>
 }
 
 export default function Board() {
-
+  const [squares ,Setsquares]=useState(Array(9).fill(null));
+  function handelclck(i){
+    const nextSquares=squares.slice();
+    nextSquares[i]="X";
+    Setsquares(nextSquares);
+ }
   return (
     <div>
       <div className="board-row">
-       <Square value={1}/>
-       <Square  value={2}/>
-       <Square  value={3}/>
-      
+      <Square value={squares[0]} onSquareClick={()=>handelclck(0)} />
+      <Square value={squares[1]} onSquareClick={()=>handelclck(1)}/>
+      <Square value={squares[2]} onSquareClick={()=>handelclck(2)}/>
       </div>
       <div className="board-row">
-      <Square value={4}/>
-       <Square  value={5}/>
-       <Square  value={6}/>
+      <Square value={squares[3]} onSquareClick={()=>handelclck(3)}/>
+      <Square value={squares[4]} onSquareClick={()=>handelclck(4)}/>
+      <Square value={squares[5]} onSquareClick={()=>handelclck(5)}/>
       </div>
       <div className="board-row">
-      <Square value={7}/>
-       <Square  value={8}/>
-       <Square  value={9}/>
+      <Square value={squares[6]} onSquareClick={()=>handelclck(6)}/>
+      <Square value={squares[7]} onSquareClick={()=>handelclck(7)}/>
+      <Square value={squares[8]} onSquareClick={()=>handelclck(8)}/>
       </div>
     </div>
   );
